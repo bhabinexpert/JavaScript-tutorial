876240ea7653b7c93188fbceef52c35ba980ac97
bhabinexpert <bhabindulal35@gmail.com>
2025-06-14 21:56:45 +0545

    object and new

--- 10_OOP/object.js
+++ 10_OOP/object.js
@@ -8,3 +8,20 @@ console.log(multiplyBy5(5))
 console.log(multiplyBy5.power)
 console.log(multiplyBy5.prototype)
 // function is also an object in js.
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.Increment = function(){
+    this.score++
+}
+
+createUser.prototype.printMe = function(){
+    console.log(`price is ${this.score}`)
+}
+
+const vabin = new createUser("bhabin", 20)
+const tea = new createUser("tea", 10)
+
+vabin.printMe()
